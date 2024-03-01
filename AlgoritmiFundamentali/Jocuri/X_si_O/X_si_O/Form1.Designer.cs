0ed7943c417db278019a29b60d108ed3846edca6
PelleRemus <remusnicolaepelle@gmail.com>
2024-03-01 13:27:34 +0200

    jocul X si O

---
+++ AlgoritmiFundamentali/Jocuri/X_si_O/X_si_O/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace X_si_O
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(618, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 623);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AlgoritmiFundamentali/Jocuri/X_si_O/X_si_O/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace X_si_O
+{
+    public partial class Form1 : Form
+    {
+        Button[,] buttons = new Button[3, 3];
+        string player = "X";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int size = panel1.Width / 3;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    Button button = new Button();
+                    button.Parent = panel1;
+                    button.Size = new Size(size, size);
+                    button.Location = new Point(j * size, i * size);
+                    button.Font = new Font("Arial", size / 3);
+                    button.Click += Button_Click;
+                    buttons[i, j] = button;
+                }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            ResetAllButtons();
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Button button = sender as Button;
+            button.Text = player;
+            button.Enabled = false;
+
+            if (IsGameWon())
+            {
+                DisableAllButtons();
+                MessageBox.Show($"Player {player} has won!", "You Won!");
+            }
+            else if (IsDraw())
+            {
+                MessageBox.Show($"No player has won this time!", "It's a Draw!");
+            }
+
+            if (player == "X")
+                player = "O";
+            else
+                player = "X";
+        }
+
+        bool IsGameWon()
+        {
+            // verificare pe linii
+            bool hasWon = buttons[0, 0].Text == player && buttons[0, 1].Text == player && buttons[0, 2].Text == player;
+            hasWon = hasWon || buttons[1, 0].Text == player && buttons[1, 1].Text == player && buttons[1, 2].Text == player;
+            hasWon = hasWon || buttons[2, 0].Text == player && buttons[2, 1].Text == player && buttons[2, 2].Text == player;
+
+            // verificare pe coloane
+            hasWon = hasWon || buttons[0, 0].Text == player && buttons[1, 0].Text == player && buttons[2, 0].Text == player;
+            hasWon = hasWon || buttons[0, 1].Text == player && buttons[1, 1].Text == player && buttons[2, 1].Text == player;
+            hasWon = hasWon || buttons[0, 2].Text == player && buttons[1, 2].Text == player && buttons[2, 2].Text == player;
+
+            // verificare pe diagonale
+            hasWon = hasWon || buttons[0, 0].Text == player && buttons[1, 1].Text == player && buttons[2, 2].Text == player;
+            hasWon = hasWon || buttons[0, 2].Text == player && buttons[1, 1].Text == player && buttons[2, 0].Text == player;
+
+            return hasWon;
+        }
+
+        bool IsDraw()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    if (buttons[i, j].Text == "")
+                        return false;
+                }
+            return true;
+        }
+
+        void DisableAllButtons()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    buttons[i, j].Enabled = false;
+        }
+
+        void ResetAllButtons()
+        {
+            player = "X";
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Enabled = true;
+                    buttons[i, j].Text = "";
+                }
+        }
+    }
+}
