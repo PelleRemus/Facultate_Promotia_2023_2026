61ca212dfc3daa85aee8dc244e7defeb4cf956b5
PelleRemus <remusnicolaepelle@gmail.com>
2024-03-12 14:31:03 +0200

    pregatire pentru laborator

---
+++ AlgoritmiFundamentali/Algoritmi/MatriciVizual/MatriciVizual/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace MatriciVizual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(720, 720);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(738, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(740, 367);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1 2 3 4 5\r\n2 3 4 5 6\r\n3 4 5 6 7\r\n4 5 6 7 8\r\n5 6 7 8 9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1490, 742);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ AlgoritmiFundamentali/Algoritmi/MatriciVizual/MatriciVizual/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MatriciVizual
+{
+    public partial class Form1 : Form
+    {
+        Color[] colors = new Color[]
+        {
+            Color.White,
+            Color.Red,
+            Color.OrangeRed,
+            Color.Orange,
+            Color.Yellow,
+            Color.YellowGreen,
+            Color.Green,
+            Color.Blue,
+            Color.Indigo,
+            Color.BlueViolet
+        };
+        Point colorsStartLocation = new Point(737, 683);
+        Button[] colorButtons;
+        PictureBox[,] matrix = new PictureBox[0, 0];
+
+        public Form1()
+        {
+            InitializeComponent();
+            colorButtons = new Button[colors.Length];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Button button = new Button();
+                button.Parent = this;
+
+                button.Size = new Size(50, 50);
+                button.Location = new Point(colorsStartLocation.X + i * 55, colorsStartLocation.Y);
+                button.BackColor = colors[i];
+
+                button.Click += ColorButton_Click;
+                colorButtons[i] = button;
+            }
+        }
+
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorPicker = new ColorDialog();
+            if (colorPicker.ShowDialog() == DialogResult.OK)
+            {
+                int index = Array.IndexOf(colorButtons, sender as Button);
+                colors[index] = colorPicker.Color;
+                (sender as Button).BackColor = colorPicker.Color;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j].Parent = null;
+
+            string[][] textBoxText = new string[textBox1.Lines.Length][];
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+                textBoxText[i] = textBox1.Lines[i].Split(' ');
+
+            int n = textBoxText.Length;
+            int m = textBoxText[0].Length;
+            matrix = new PictureBox[n, m];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = new PictureBox();
+                    matrix[i, j].Parent = pictureBox1;
+
+                    int sizeX = pictureBox1.Width / m, sizeY = pictureBox1.Height / n;
+                    matrix[i, j].Size = new Size(sizeX, sizeY);
+                    matrix[i, j].Location = new Point(j * sizeX, i * sizeY);
+
+                    int index = int.Parse(textBoxText[i][j]) % colors.Length;
+                    matrix[i, j].BackColor = colors[index];
+                }
+        }
+
+        private void AddMatrixToTextBox(int[,] matrix, int n, int m)
+        {
+            textBox1.Text = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m - 1; j++)
+                    textBox1.Text += matrix[i, j] + " ";
+                textBox1.Text += matrix[i, m - 1];
+                if (i < n - 1)
+                    textBox1.Text += Environment.NewLine;
+            }
+        }
+    }
+}
