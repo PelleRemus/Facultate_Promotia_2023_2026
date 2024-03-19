9102f915e4ff04f82004e0ea5514fddb35915070
PelleRemus <remusnicolaepelle@gmail.com>
2024-03-19 20:00:56 +0200

    Laborator 19.03.2024 - am inceput jocul 2048. Am generat tile-urile vizual si le-am facut sa mearga spre dreapta.

---
+++ AlgoritmiFundamentali/Jocuri/The_2048_Game/The_2048_Game/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace The_2048_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(168)))), ((int)(((byte)(156)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(518, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 522);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AlgoritmiFundamentali/Jocuri/The_2048_Game/The_2048_Game/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace The_2048_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int n = 4;
+        int[,] values;
+        PictureBox[,] matrix;
+        Random random = new Random();
+        Color emptySpace = Color.FromArgb(195, 188, 176);
+        Color[] colors = new Color[]
+        {
+            Color.FromArgb(237, 228, 219),
+            Color.FromArgb(237, 224, 201),
+            Color.FromArgb(244, 177, 122),
+            Color.FromArgb(247, 150, 99),
+            Color.FromArgb(246, 125, 98),
+            Color.FromArgb(246, 94, 57),
+            Color.FromArgb(237, 206, 115),
+            Color.FromArgb(237, 202, 100),
+            Color.FromArgb(237, 198, 81),
+            Color.FromArgb(238, 199, 68),
+            Color.FromArgb(236, 194, 48),
+            Color.FromArgb(255, 32, 33),
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            matrix = new PictureBox[n, n];
+            int size = (panel1.Width - 10) / n - 10;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    PictureBox picturebox = new PictureBox();
+                    picturebox.Parent = panel1;
+                    picturebox.BackColor = emptySpace;
+                    picturebox.Size = new Size(size, size);
+                    picturebox.Location = new Point(10 + j * (size + 10), 10 + i * (size + 10));
+
+                    matrix[i, j] = picturebox;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            values = new int[n, n];
+            GenerateNewTile();
+            GenerateNewTile();
+            Render();
+        }
+
+        private void GenerateNewTile()
+        {
+            int i, j;
+            do
+            {
+                i = random.Next(n);
+                j = random.Next(n);
+            } while (values[i, j] != 0);
+
+            values[i, j] = 1;
+        }
+
+        private void Render()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (values[i, j] == 0)
+                    {
+                        matrix[i, j].BackColor = emptySpace;
+                        continue;
+                    }
+
+                    int index = values[i, j] - 1;
+                    matrix[i, j].BackColor = colors[index];
+                }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (values == null)
+                return;
+
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                MoveLeft();
+                GenerateNewTile();
+            }
+            Render();
+        }
+
+        private void MoveLeft()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 1; j < n; j++)
+                {
+                    int index = j;
+                    while (index > 0 && values[i, index - 1] == 0)
+                    {
+                        values[i, index - 1] = values[i, index];
+                        values[i, index] = 0;
+                        index--;
+                    }
+                }
+        }
+    }
+}
