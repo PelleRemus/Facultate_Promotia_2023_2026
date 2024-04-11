502812011d264673dabd6b89efea4521b432e94f
PelleRemus <remusnicolaepelle@gmail.com>
2024-04-11 19:43:09 +0300

    Laborator 11.04.2024

---
+++ AlgoritmiFundamentali/Algoritmi/ReversePolishNotation/ReversePolishNotation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ReversePolishNotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verificam daca Stackk merge
+            /*Stackk stack = new Stackk();
+
+            stack.AddEnd(3);
+            stack.AddEnd(2);
+            stack.AddEnd(4);
+
+            for (int i = 0; i < stack.Length; i++)
+                Console.Write(stack.Values[i] + " ");
+
+            Console.WriteLine();
+            Console.WriteLine(stack.DeleteEnd());
+
+            for (int i = 0; i < stack.Length; i++)
+                Console.Write(stack.Values[i] + " ");*/
+
+            Stackk stack = new Stackk();
+            string reversePolishNotationEquation = Console.ReadLine();
+            string[] elements = reversePolishNotationEquation.Split(' ');
+
+            foreach (string element in elements)
+            {
+                if (int.TryParse(element, out int value))
+                {
+                    // Ce se intampla in spatele metodei TryParse(string s, out int result)
+                    /*try
+                    {
+                        result = int.Parse(s);
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }*/
+                    stack.AddEnd(value);
+                }
+                else
+                {
+                    ApplyOperation(stack, element);
+                }
+            }
+
+            if (stack.Length == 1)
+                Console.WriteLine($"Rezultatul operatiei este {stack.DeleteEnd()}");
+            else
+                Console.WriteLine("Verificati input-ul, ecuatia nu este rezolvabila!");
+
+            Console.ReadKey();
+        }
+
+        static void ApplyOperation(Stackk stack, string operation)
+        {
+            int value2 = stack.DeleteEnd();
+            int value1 = stack.DeleteEnd();
+            switch (operation)
+            {
+                case "+": stack.AddEnd(value1 + value2); break;
+                case "-": stack.AddEnd(value1 - value2); break;
+                case "*": stack.AddEnd(value1 * value2); break;
+                case "/": stack.AddEnd(value1 / value2); break;
+            }
+        }
+    }
+}
---
+++ AlgoritmiFundamentali/Algoritmi/ReversePolishNotation/ReversePolishNotation/Stackk.cs
@@ -0,0 +1,39 @@
+﻿namespace ReversePolishNotation
+{
+    public class Stackk
+    {
+        public int[] Values { get; set; }
+        public int Length { get; set; }
+
+        public Stackk()
+        {
+            Length = 0;
+            Values = new int[Length];
+        }
+
+        public void AddEnd(int value)
+        {
+            Length++;
+            int[] values = new int[Length];
+
+            for (int i = 0; i < Length - 1; i++)
+                values[i] = Values[i];
+
+            values[Length - 1] = value;
+            Values = values;
+        }
+
+        public int DeleteEnd()
+        {
+            Length--;
+            int[] values = new int[Length];
+            int value = Values[Length];
+
+            for (int i = 0; i < Length; i++)
+                values[i] = Values[i];
+
+            Values = values;
+            return value;
+        }
+    }
+}
