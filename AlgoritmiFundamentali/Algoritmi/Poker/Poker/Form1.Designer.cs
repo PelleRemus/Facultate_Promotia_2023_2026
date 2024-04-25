18fac048c6c85d5907877ace5edcfbcc6f79a98e
PelleRemus <remusnicolaepelle@gmail.com>
2024-04-25 12:06:37 +0300

    Poker - pregatire pentru laborator

---
+++ AlgoritmiFundamentali/Algoritmi/Poker/Poker/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Poker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.p1_1 = new System.Windows.Forms.PictureBox();
+            this.p1_2 = new System.Windows.Forms.PictureBox();
+            this.p1_3 = new System.Windows.Forms.PictureBox();
+            this.p1_5 = new System.Windows.Forms.PictureBox();
+            this.p1_4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.p2_4 = new System.Windows.Forms.PictureBox();
+            this.p2_5 = new System.Windows.Forms.PictureBox();
+            this.p2_3 = new System.Windows.Forms.PictureBox();
+            this.p2_2 = new System.Windows.Forms.PictureBox();
+            this.p2_1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(575, 33);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 162);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(602, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dealer";
+            // 
+            // p1_1
+            // 
+            this.p1_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p1_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1_1.Location = new System.Drawing.Point(9, 276);
+            this.p1_1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p1_1.Name = "p1_1";
+            this.p1_1.Size = new System.Drawing.Size(110, 162);
+            this.p1_1.TabIndex = 2;
+            this.p1_1.TabStop = false;
+            // 
+            // p1_2
+            // 
+            this.p1_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p1_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1_2.Location = new System.Drawing.Point(124, 276);
+            this.p1_2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p1_2.Name = "p1_2";
+            this.p1_2.Size = new System.Drawing.Size(110, 162);
+            this.p1_2.TabIndex = 3;
+            this.p1_2.TabStop = false;
+            // 
+            // p1_3
+            // 
+            this.p1_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p1_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1_3.Location = new System.Drawing.Point(238, 276);
+            this.p1_3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p1_3.Name = "p1_3";
+            this.p1_3.Size = new System.Drawing.Size(110, 162);
+            this.p1_3.TabIndex = 4;
+            this.p1_3.TabStop = false;
+            // 
+            // p1_5
+            // 
+            this.p1_5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p1_5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1_5.Location = new System.Drawing.Point(468, 276);
+            this.p1_5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p1_5.Name = "p1_5";
+            this.p1_5.Size = new System.Drawing.Size(110, 162);
+            this.p1_5.TabIndex = 5;
+            this.p1_5.TabStop = false;
+            // 
+            // p1_4
+            // 
+            this.p1_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p1_4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p1_4.Location = new System.Drawing.Point(353, 276);
+            this.p1_4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p1_4.Name = "p1_4";
+            this.p1_4.Size = new System.Drawing.Size(110, 162);
+            this.p1_4.TabIndex = 6;
+            this.p1_4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(262, 250);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Player1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(935, 250);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Player2";
+            // 
+            // p2_4
+            // 
+            this.p2_4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p2_4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2_4.Location = new System.Drawing.Point(1028, 276);
+            this.p2_4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p2_4.Name = "p2_4";
+            this.p2_4.Size = new System.Drawing.Size(110, 162);
+            this.p2_4.TabIndex = 18;
+            this.p2_4.TabStop = false;
+            // 
+            // p2_5
+            // 
+            this.p2_5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p2_5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2_5.Location = new System.Drawing.Point(1142, 276);
+            this.p2_5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p2_5.Name = "p2_5";
+            this.p2_5.Size = new System.Drawing.Size(110, 162);
+            this.p2_5.TabIndex = 17;
+            this.p2_5.TabStop = false;
+            // 
+            // p2_3
+            // 
+            this.p2_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p2_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2_3.Location = new System.Drawing.Point(913, 276);
+            this.p2_3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p2_3.Name = "p2_3";
+            this.p2_3.Size = new System.Drawing.Size(110, 162);
+            this.p2_3.TabIndex = 16;
+            this.p2_3.TabStop = false;
+            // 
+            // p2_2
+            // 
+            this.p2_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p2_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2_2.Location = new System.Drawing.Point(798, 276);
+            this.p2_2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p2_2.Name = "p2_2";
+            this.p2_2.Size = new System.Drawing.Size(110, 162);
+            this.p2_2.TabIndex = 15;
+            this.p2_2.TabStop = false;
+            // 
+            // p2_1
+            // 
+            this.p2_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(156)))), ((int)(((byte)(67)))));
+            this.p2_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.p2_1.Location = new System.Drawing.Point(683, 276);
+            this.p2_1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.p2_1.Name = "p2_1";
+            this.p2_1.Size = new System.Drawing.Size(110, 162);
+            this.p2_1.TabIndex = 14;
+            this.p2_1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(575, 201);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 32);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Deal Cards";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(575, 488);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 32);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Check Hands";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(1262, 531);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.p2_4);
+            this.Controls.Add(this.p2_5);
+            this.Controls.Add(this.p2_3);
+            this.Controls.Add(this.p2_2);
+            this.Controls.Add(this.p2_1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.p1_4);
+            this.Controls.Add(this.p1_5);
+            this.Controls.Add(this.p1_3);
+            this.Controls.Add(this.p1_2);
+            this.Controls.Add(this.p1_1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Poker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox p1_1;
+        private System.Windows.Forms.PictureBox p1_2;
+        private System.Windows.Forms.PictureBox p1_3;
+        private System.Windows.Forms.PictureBox p1_5;
+        private System.Windows.Forms.PictureBox p1_4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox p2_4;
+        private System.Windows.Forms.PictureBox p2_5;
+        private System.Windows.Forms.PictureBox p2_3;
+        private System.Windows.Forms.PictureBox p2_2;
+        private System.Windows.Forms.PictureBox p2_1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
