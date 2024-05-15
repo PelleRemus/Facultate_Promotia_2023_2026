9ef41b5c3bf72cc8fc80e174c3bfcac4be39efcb
PelleRemus <remusnicolaepelle@gmail.com>
2024-05-15 23:37:40 +0300

    Laborator 09.05.2024 - Divide and Conquer si aplicatie vizuala pentru Turnurile lui Hanoi

---
+++ AlgoritmiFundamentali/Algoritmi/DivideAndConquer/DivideAndConquer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DivideAndConquer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine(BinarySearch(array, 0, array.Length, n));
+            string[] array = { "apa", "banana", "carte", "mijloc", "zebra" };
+            string word = Console.ReadLine();
+            Console.WriteLine(SearchInDictionary(array, 0, array.Length, word));
+            Console.ReadKey();
+        }
+
+        // Se da o lista de numere ordonate.
+        // Sa se afle daca un numar n este inclus in aceasta lista
+        static bool BinarySearch(int[] array, int left, int right, int n)
+        {
+            if (left > right)
+                return false;
+
+            int middle = (left + right) / 2;
+            if (array[middle] == n)
+                return true;
+
+            if (array[middle] < n)
+                return BinarySearch(array, middle + 1, right, n);
+            else
+                return BinarySearch(array, left, middle - 1, n);
+        }
+
+        // Cum ati cauta un cuvant intr-un dictionar?
+        static bool SearchInDictionary(string[] array, int left, int right, string word)
+        {
+            if (left > right)
+                return false;
+
+            int middle = (left + right) / 2;
+            if (array[middle].CompareTo(word) == 0)
+                return true;
+
+            if (array[middle].CompareTo(word) < 0)
+                return SearchInDictionary(array, middle + 1, right, word);
+            else
+                return SearchInDictionary(array, left, middle - 1, word);
+        }
+    }
+}
---
+++ AlgoritmiFundamentali/Algoritmi/TurnuriHanoi/TurnuriHanoi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TurnuriHanoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Width = this.Width;
+            Engine.Init(this);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                var messageBoxResult = MessageBox.Show("Are you sure you want to Exit the game?",
+                    "Exit game?", MessageBoxButtons.OKCancel);
+                if (messageBoxResult == DialogResult.OK)
+                    this.Close();
+            }
+        }
+
+        private void NrOfDisks_ValueChanged(object sender, EventArgs e)
+        {
+            int nrOfDisks = (int)NrOfDisks.Value;
+            Engine.InitGame(nrOfDisks);
+            MinimumMoves.Text = "Minimum Moves: " + (Math.Pow(2, nrOfDisks) - 1);
+        }
+
+        private async void Solve_Click(object sender, EventArgs e)
+        {
+            await Engine.SolveRecursive((int)NrOfDisks.Value, Engine.A, Engine.B, Engine.C);
+        }
+    }
+}
