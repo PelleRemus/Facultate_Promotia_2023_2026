128c5404f96f9c3aed8cd0df996725eec740a89d
PelleRemus <remusnicolaepelle@gmail.com>
2024-05-16 23:22:28 +0300

    Laborator 16.05.2024

---
+++ AlgoritmiFundamentali/Algoritmi/ArieMinimaPuncte/ArieMinimaPuncte/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ArieMinimaPuncte
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 700);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1018, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate Points";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Crimson;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1018, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(1018, 333);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(260, 60);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Min Triangle";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(1116, 459);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 45);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(1018, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Area:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(1018, 399);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(260, 60);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Poligon";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold);
+            this.checkBox1.Location = new System.Drawing.Point(1072, 520);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 42);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Shuffle";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1290, 723);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ AlgoritmiFundamentali/Algoritmi/ArieMinimaPuncte/ArieMinimaPuncte/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ArieMinimaPuncte
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        Graphics graphics;
+        Bitmap bitmap;
+        List<Point> points = new List<Point>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Cand generam puncte, putem calcula aria triungiului minim si a poligonului
+            button3.Enabled = button4.Enabled = true;
+            for (int i = 0; i < 10; i++)
+            {
+                // Generam 10 puncte la intamplare
+                int x = random.Next(pictureBox1.Width);
+                int y = random.Next(pictureBox1.Height);
+                points.Add(new Point(x, y));
+            }
+            DisplayPoints();
+        }
+
+        void DisplayPoints()
+        {
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                // Pentru a afisa punctele, desenam cercuri de dimensiunea 11 pentru a le observa usor
+                graphics.FillEllipse(new SolidBrush(Color.Black),
+                    points[i].X - 5, points[i].Y - 5, 11, 11);
+            }
+
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            DisplayPoints();
+            // Cand stergem toate punctele din lista,
+            // nu mai putem da click pe butoanele care calculeaza ariile
+            button3.Enabled = button4.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            float min = float.MaxValue;
+            Point[] minTriangle = new Point[3];
+
+            // Parcurgem astfel incat sa generam toate combinarile posibile
+            for (int i = 0; i < points.Count - 2; i++)
+                for (int j = i + 1; j < points.Count - 1; j++)
+                    for (int k = j + 1; k < points.Count; k++)
+                    {
+                        // Calculam aria si verificam daca am gasit un nou triunghi minim
+                        float area = Area(points[i], points[j], points[k]);
+                        if (area < min)
+                        {
+                            min = area;
+                            minTriangle[0] = points[i];
+                            minTriangle[1] = points[j];
+                            minTriangle[2] = points[k];
+                        }
+                    }
+            // Afisam aria minima
+            textBox1.Text = min.ToString();
+            // Si afisam triunghiul cu aceasta arie
+            graphics.FillPolygon(new SolidBrush(Color.ForestGreen), minTriangle);
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Point[] pointsArray = points.ToArray();
+            if (checkBox1.Checked)
+                Shuffle(pointsArray);
+            textBox1.Text = Area(pointsArray).ToString();
+
+            DisplayPoints();
+            graphics.FillPolygon(new SolidBrush(Color.ForestGreen), pointsArray);
+            pictureBox1.Image = bitmap;
+        }
+
+        void Shuffle(Point[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                int j = random.Next(v.Length);
+                Point aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+            }
+        }
+
+        // Formula pentru aria triunghiului bazata pe cele 3 coordonate ale punctelor
+        float Area(Point A, Point B, Point C)
+        {
+            return Math.Abs(A.X * (B.Y - C.Y) + B.X * (C.Y - A.Y)
+                + C.X * (A.Y - B.Y)) * 0.5F;
+        }
+
+        // Formula pentru aria unui poligon
+        float Area(Point[] p)
+        {
+            float s = 0;
+            for (int i = 0; i < p.Length - 1; i++)
+                s += 0.5f * (p[i].X * p[i + 1].Y - p[i + 1].X * p[i].Y);
+            s += 0.5f * (p[p.Length - 1].X * p[0].Y - p[0].X * p[p.Length - 1].Y);
+            return Math.Abs(s);
+        }
+    }
+}
